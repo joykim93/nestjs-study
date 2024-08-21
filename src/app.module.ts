9cc459e3e95e90f444cd1070ca0b62fce595aad8
joykim93 <joykim9311@gmail.com>
2024-08-22 00:39:36 +0900

    feat: typeorm 기본적인 설정 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'fastcampus',
+      password: 'fastcampus1234',
+      database: 'postgres',
+      entities: [__dirname + '/**/*.entity.{.ts,.js}'],
+      synchronize: false,
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
